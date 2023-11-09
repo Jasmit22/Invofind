88b6dabc48f144f2e3d4b9bf31a6092b99ad3530
Jasmit <jasmit2294@protonmail.com>
2023-11-08 23:07:55 -0700

    progress

--- invofindFE/part1/src/App.jsx
+++ invofindFE/part1/src/App.jsx
@@ -1,36 +1,77 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
+import Togglable from "./components/Togglable";
+import LoginForm from "./components/LoginForm";
 
-import "./App.css"
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
-    const invofindHeading = document.querySelector(".invofindHeading")
-    invofindHeading.innerHTML = invofindHeading.innerText
-      .split("")
-      .map(
-        (letters, i) =>
-          `<span style = "transition-delay:${i * 30}ms;
+    const invofindHeading = document.querySelector(".invofindHeading");
+    if (invofindHeading) {
+      invofindHeading.innerHTML = invofindHeading.innerText
+        .split("")
+        .map(
+          (letters, i) =>
+            `<span style = "transition-delay:${i * 30}ms;
           filter: hue-rotate(${i * 15}deg)"›$">${letters}</span>`
-      )
-      .join("")
-  }, [])
+        )
+        .join("");
+    }
+  }, []);
+
+  const handleLogin = async (event) => {
+    // event.preventDefault();
+    // try {
+    //   const user = await loginService.login({
+    //     username,
+    //     password,
+    //   });
+    //   blogService.setToken(user.token);
+    //   window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
+    //   setUser(user);
+    //   setUsername("");
+    //   setPassword("");
+    // } catch (exception) {
+    //   setErrorMessage(exception.response.data.error);
+    //   setTimeout(() => {
+    //     setErrorMessage(null);
+    //   }, 4000);
+    //   console.log(exception);
+    // }
+  };
+
+  const loginForm = () => {
+    return (
+      <div>
+        <Togglable buttonLabel="Log In">
+          <LoginForm
+            handleSubmit={handleLogin}
+            handleUsernameChange={({ target }) => setUsername(target.value)}
+            handlePasswordChange={({ target }) => setPassword(target.value)}
+            username={username}
+            password={password}
+          />
+        </Togglable>
+      </div>
+    );
+  };
 
   return (
     <div>
       <div className="banner">
-        <div className='invofindHeading'>
-      InvoFind 🔍
-        </div>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-          Log in {count}
-          </button>
+        <div className="invofindHeading">InvoFind 🔍</div>
+        <div className="logOut">
+          <button onClick={() => {}}>Log out</button>
         </div>
       </div>
+      <div className="content">{user === null && loginForm()}</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ invofindFE/part1/src/components/LoginForm.jsx
@@ -0,0 +1,48 @@
+import PropTypes from "prop-types";
+
+const LoginForm = ({
+  handleSubmit,
+  handleUsernameChange,
+  handlePasswordChange,
+  username,
+  password,
+}) => {
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Username
+          <input
+            value={username}
+            onChange={handleUsernameChange}
+            style={{ marginLeft: "10px" }}
+          />
+        </div>
+        <div>
+          Password
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            style={{ marginLeft: "10px" }}
+          />
+        </div>
+        <button className="submitButton" type="submit">
+          Login
+        </button>
+        <br></br>
+      </form>
+    </div>
+  );
+};
+
+LoginForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+};
+
+export default LoginForm;
